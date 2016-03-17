2cd1ba38aa929a5315b8fcb53348ab27e36e6384
marcin <marcin@freeportmetrics.com>
2016-03-17 13:32:26 +0100

    Beacons configurations

--- app/src/main/java/com/freeportmetrics/beaconf/BeconfApplication.java
+++ app/src/main/java/com/freeportmetrics/beaconf/BeconfApplication.java
@@ -1,7 +1,11 @@
 package com.freeportmetrics.beaconf;
 
 import android.app.Application;
+import android.os.RemoteException;
 import android.util.Log;
+
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
 
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
@@ -10,9 +14,15 @@ import org.altbeacon.beacon.Region;
 import org.altbeacon.beacon.startup.BootstrapNotifier;
 import org.altbeacon.beacon.startup.RegionBootstrap;
 
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BeconfApplication extends Application implements BootstrapNotifier {
     private static final String TAG = "BeconfApplication";
     private RegionBootstrap regionBootstrap;
+
+    private Socket mSocket;
 
     @Override
     public void onCreate() {
@@ -22,9 +32,32 @@ public class BeconfApplication extends Application implements BootstrapNotifier 
         beaconManager = BeaconManager.getInstanceForApplication(getBaseContext());
         beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
 
-        // TODO: remaining regions
-        Region blueberry = new Region("com.freeportmetrics.blueberry", Identifier.parse("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), Identifier.fromInt(10344), Identifier.fromInt(31183));
-        regionBootstrap = new RegionBootstrap(this, blueberry);
+        beaconManager.setBackgroundScanPeriod(1000l);
+        beaconManager.setBackgroundBetweenScanPeriod(1000l);
+        beaconManager.setForegroundBetweenScanPeriod(1000l);
+        beaconManager.setForegroundScanPeriod(1000l);
+
+        Region icy = new Region("com.freeportmetrics.icy", Identifier.parse("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), Identifier.fromInt(45287), Identifier.fromInt(53858));
+        Region blueBerry = new Region("com.freeportmetrics.blueBerry", Identifier.parse("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), Identifier.fromInt(10344), Identifier.fromInt(31183));
+        Region mint = new Region("com.freeportmetrics.mint", Identifier.parse("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), Identifier.fromInt(5919), Identifier.fromInt(60231));
+
+        List<Region> regions = new ArrayList<Region>();
+        regions.add(icy);
+        regions.add(blueBerry);
+        regions.add(mint);
+
+        regionBootstrap = new RegionBootstrap(this, regions);
+
+        try {
+            IO.Options opts = new IO.Options();
+            opts.reconnectionAttempts = 3;
+            opts.reconnectionDelay = 3000;
+            opts.forceNew = true;
+            mSocket = IO.socket("http://beatconf-freeportmetrics.rhcloud.com", opts);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        mSocket.connect();
     }
 
     @Override
@@ -36,7 +69,8 @@ public class BeconfApplication extends Application implements BootstrapNotifier 
     public void didEnterRegion(Region arg0) {
         String userId = Utils.getDefaults(Utils.USER_ID_PREF_KEY, this);
         String regionId = arg0.getId2()+"_"+arg0.getId3();
-        Log.i(TAG, "Got a didEnterRegion call for regionId: "+regionId);
+        Log.i(TAG, "Got a didEnterRegion call for regionId: " + regionId);
+        emitEnterRoomEvent(regionId);
     }
 
     @Override
@@ -44,5 +78,20 @@ public class BeconfApplication extends Application implements BootstrapNotifier 
         String userId = Utils.getDefaults(Utils.USER_ID_PREF_KEY, this);
         String regionId = arg0.getId2()+"_"+arg0.getId3();
         Log.i(TAG, "Got a didExitRegion call for regionId: "+regionId);
+        emitLeaveRoomEvent(regionId);
+    }
+
+    public void emitEnterRoomEvent(String roomId){
+        String userId = Utils.getDefaults(Utils.USER_ID_PREF_KEY, this);
+        Log.i(TAG, "Sending enter event to server, userId: " + userId + ", roomId: " + roomId);
+        //debug("Sending enter event to server, userId: " + userId + ", roomId: " + roomId);
+        mSocket.emit("enterRoom", "{\"user_id\":\"" + userId + "\",\"room_id\":\"" + roomId + "\"}");
+    }
+
+    public void emitLeaveRoomEvent(String roomId){
+        String userId = Utils.getDefaults(Utils.USER_ID_PREF_KEY, this);
+        Log.i(TAG, "Sending leave event to server, userId: " + userId + ", roomId: " + roomId);
+        //debug("Sending leave event to server, userId: " + userId + ", roomId: " + roomId);
+        mSocket.emit("leaveRoom", "{\"user_id\":\"" + userId + "\",\"room_id\":\"" + roomId + "\"}");
     }
 }
